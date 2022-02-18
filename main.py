a53032f4e580838e2d773f66858dec229d737e51
lucadenhez <lucadenhez@icloud.com>
2022-02-18 00:46:39 -0800

    Added save feature, to avoid losing progress

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ import sounddevice as sd
 import soundfile as sf
 from scipy.io.wavfile import write
 import re, queue, sys, tempfile, numpy, threading
-from os import system
+from os import system, path, remove
 
 assert numpy
 
 TRANSCRIPT = "transcript.txt"
-AUDIO_PATH = "audio/"
-TEXT_FILE = "text/natgeo.txt"
+AUDIO_PATH = "audio"
+TEXT_FILE = path.join("text", "natgeo.txt")
+LOG_FILE = "log.txt"
 
 q = queue.Queue()
 
 def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
     if status:
-        print(status, file=sys.stderr)
+        print(status, file = sys.stderr)
     q.put(indata.copy())
 
+# AI Model must be mono, with a frequency of 22 khz
 def recordAudio(i, running):
-   with sf.SoundFile(AUDIO_PATH + str(i) + ".wav", mode = 'x', samplerate = 22000,
+   with sf.SoundFile(path.join(AUDIO_PATH, str(i) + ".wav"), mode = 'x', samplerate = 22000,
                       channels = 1) as file:
         with sd.InputStream(samplerate = 22000, device = 1,
                             channels = 1, callback = callback):
@@ -33,19 +34,59 @@ with open(TEXT_FILE, 'r', encoding = "utf8") as f:
 
 sentences = [sentences[i] + sentences[i + 1] + " " for i in range(0, len(sentences), 2)]
 
-input("[!] Press enter to start recording...")
+print("[*] This bot aims to help you more efficiently train your machine learning audio model.")
+input("[!] Press enter to start recording...\n")
 
-for i in range(len(sentences)):
-    system("cls")
+startIndex = 0
 
-    print(sentences[i] + "\n")
-    
-    running = threading.Event()
-    running.set()
+with open(LOG_FILE, 'r') as f:
+    firstLine = f.readline()
 
-    t = threading.Thread(target = recordAudio, args = (i, running,))
-    t.start()
+    if firstLine != "":
+        answer = input("[!] You have already recorded audio. Do you want to continue from that point or restart? (y/n) ")
+        
+        if answer == "y":
+            startIndex = int(firstLine)
+        elif answer == "n":
+            pass
+        else:
+            print("\nAn unknown answer was given. Exiting...")
+            sys.exit()
 
-    input("[*] Press enter to continue to next sentence...")
-    running.clear()
-    t.join()
+for i in range(startIndex, len(sentences)):
+    try:
+        transcript = open(TRANSCRIPT, 'a', encoding = "utf8")
+        log = open(LOG_FILE, 'w', encoding = "utf8")
+
+        system("cls")
+
+        print(("#" * 20) + " SENTENCE " + str(i) + " " + ("#" * 20))
+        print("\n" + sentences[i] + "\n")
+        print("#" * 50)
+        print("")
+
+        if path.isfile(path.join(AUDIO_PATH, str(i) + ".wav")):
+            remove(path.join(AUDIO_PATH, str(i) + ".wav"))
+
+        running = threading.Event()
+        running.set()
+        t = threading.Thread(target = recordAudio, args = (i, running,))
+        t.start()
+
+        input("[-->] Press enter to continue to next sentence...")
+        
+        running.clear()
+        t.join()
+
+        transcript.write(str(path.join(AUDIO_PATH, str(i) + ".wav")) + "|" + sentences[i] + "\n")
+        log.write(str(i) + "\n")
+
+    except KeyboardInterrupt:
+        print("\n\n[!] Exiting...\n")
+        
+        running.clear()
+        t.join()
+        break
+
+transcript.close()
+log.close()
